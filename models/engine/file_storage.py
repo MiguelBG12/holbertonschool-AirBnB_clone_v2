ae44d931db24c4f5f8e5a1c31367092738dbbe5b
MiguelBG12 <miguel.bg12@gmail.com>
2024-01-22 20:34:47 -0500

    Now the prototype of def all(self) to def all(self, cls=None) - that returns the list of objects of one type of class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,11 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is not None:
+            return {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
+        return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
