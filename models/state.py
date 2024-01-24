ec95ca2f6d702d39a7a288d54824293359fbc3d4
MiguelBG12 <miguel.bg12@gmail.com>
2024-01-23 23:02:27 -0500

    Fix code bug

--- models/state.py
+++ models/state.py
@@ -2,30 +2,33 @@
 """ State Module for HBNB project """
 import models
 from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 
-class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = "states"
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    class State(BaseModel, Base):
+        """ State class """
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship(
+            "City", backref="state", cascade="all, delete-orphan"
+        )
+else:
+    class State(BaseModel):
+        """ State class """
+        name = ""
 
-    name = Column(String(128), nullable=False)
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City", cascade="all, delete", backref="state")
-    else:
-        @property
-        def cities(self):
-            """ Getter attribute cities for FileStorage """
-            list_cities = []
-            
-            # Check if City class is defined in the models module
-            if 'City' in dir(models):
-                City = models.City
-
-                # Filter cities with matching state_id
-                list_cities = [city for city in models.storage.all(City).values() if city.state_id == self.id]
-
-            return list_cities
+        if getenv('HBNB_TYPE_STORAGE') != 'db':
+            @property
+            def cities(self):
+                """Getter attribute para devolver la lista de objetos
+                City vinculados al Estado actual
+                """
+                city_list = []
+                for city in models.storage.all(City).values():
+                    if city.state_id == self.id:
+                        city_list.append(city)
+                return city_list
 
