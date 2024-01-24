2556e31c3b774ab5b284644aa720162aaf8d874c
DenzelRomero18 <d.romero18rb@gmail.com>
2024-01-24 13:38:16 -0500

    Adds if and else conditions to the base model file, also new import

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from os import getenv
 import models
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
@@ -11,30 +12,30 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
-
-    id = Column(String(60), unique=True, nullable=False, primary_key=True)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
-
-    def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            for key, value in kwargs.items():
-                if '__class__' not in key:
-                    setattr(self, key, value)
-
-            if not self.id:
+    if getenv ('HBNB_TYPE_STORAGE') == 'db':
+        id = Column(String(60), unique=True, nullable=False, primary_key=True)
+        created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+        updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    else:
+        def __init__(self, *args, **kwargs):
+            """Instatntiates a new model"""
+            if not kwargs:
+                from models import storage
                 self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+            else:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                del kwargs['__class__']
+                for key, value in kwargs.items():
+                    if '__class__' not in key:
+                        setattr(self, key, value)
+
+                if not self.id:
+                    self.id = str(uuid.uuid4())
 
     def __str__(self):
         """Returns a string representation of the instance"""
