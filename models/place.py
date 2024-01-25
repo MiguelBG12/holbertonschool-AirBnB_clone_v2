e93fe4b60d9ac750b5390bb1a17f76e0a3f13045
Denzel18rb <74129875@gmail.com>
2024-01-24 22:09:34 -0500

    Adds relationship between amenity and place

--- models/place.py
+++ models/place.py
@@ -3,7 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
-
+import os
+from models.amenity import Amenity
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -19,4 +20,29 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = []
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenity = Table('place_amenity', Base.metadata,
+                              Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+                              Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
+
+        amenities = relationship('Amenity', secondary=place_amenity, viewonly=False)
+
+    elif os.getenv('HBNB_TYPE_STORAGE') == 'file':
+        @property
+        def amenities(self):
+            """Getter attribute for amenities"""
+            from models import storage
+            amenities_list = []
+            for amenity_id in self.amenity_ids:
+                amenity = storage.get('Amenity', amenity_id)
+                if amenity:
+                    amenities_list.append(amenity)
+            return amenities_list
+
+        @amenities.setter
+        def amenities(self, obj):
+            """Setter attribute for amenities"""
+            if isinstance(obj, Amenity):
+                self.amenity_ids.append(obj.id)
+                self.save()
