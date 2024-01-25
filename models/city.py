fc130ea2fb31018e59d4adaa16c5e111f719db58
Denzel18rb <74129875@gmail.com>
2024-01-24 21:09:37 -0500

    Correct city class

--- models/city.py
+++ models/city.py
@@ -6,16 +6,15 @@ from os import getenv
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
-    class City(BaseModel, Base):
-        """ The city class, contains state ID and name """
-        __tablename__ = 'cities'
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name """
+    __tablename__ = "cities"
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
         name = Column(String(128), nullable=False)
-        places = relationship('Place', cascade='all, delete-orphan', back_populates='city')
-
-else:
-    class City(BaseModel):
+        places = relationship("Place", backref="cities", cascade="delete")
+    else:
         state_id = ""
         name = ""
 
