befcb915ddda7452e03fda735801a76b42cde2ff
MiguelBG12 <miguel.bg12@gmail.com>
2024-02-07 21:23:59 -0500

    Fix relationship

--- models/__init__.py
+++ models/__init__.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-import os
-from models.state import State
-from models.city import City
-from models.user import User
-from models.place import Place
-from models.amenity import Amenity
+"""This module instantiates an object of class FileStorage or DBStorage"""
 
-storage_type = os.getenv('HBNB_TYPE_STORAGE')
+from os import getenv
 
-if storage_type == "db":
+if getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
+    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-
-storage.reload()
+    storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-import os
 import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
-
-class State(BaseModel):
-    """ State class """
-    name = ""
-
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    class State(BaseModel, Base):
+        """ State class """
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="all, delete")
+        cities = relationship(
+            "City", backref="state", cascade="all, delete-orphan"
+        )
+else:
+    class State(BaseModel):
+        """ State class """
+        name = ""
 
-    else:
-        @property
-        def cities(self):
-            """ Getter attribute to return the list of instances from City """
-            from models.city import City
-            list_cities = []
-            for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    list_cities.append(city)
-            return list_cities
+        if getenv('HBNB_TYPE_STORAGE') != 'db':
+            @property
+            def cities(self):
+                """Getter attribute para devolver la lista de objetos
+                City vinculados al Estado actual
+                """
+                city_list = []
+                for city in models.storage.all(City).values():
+                    if city.state_id == self.id:
+                        city_list.append(city)
+                return city_list
