00912a0d7d87efbed95461a4ea27abd1b62db2b8
Denzel18rb <74129875rb@gmail.com>
2024-02-13 15:43:27 -0500

    updated file

--- models/state.py
+++ models/state.py
@@ -1,34 +1,49 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
-from models.base_model import BaseModel, Base
-from models.city import City
-from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
+import models
+import shlex
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
-    class State(BaseModel, Base):
-        """ State class """
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship(
-            "City", backref="state", cascade="all, delete-orphan"
-        )
-else:
-    class State(BaseModel):
-        """ State class """
-        name = ""
 
-        if getenv('HBNB_TYPE_STORAGE') != 'db':
-            @property
-            def cities(self):
-                """Getter attribute para devolver la lista de objetos
-                City vinculados al Estado actual
-                """
-                city_list = []
-                for city in models.storage.all(City).values():
-                    if city.state_id == self.id:
-                        city_list.append(city)
-                return city_list
+class State(BaseModel, Base):
+    """
+    Represents a state for a MySQL database.
 
+    Inherits from SQLAlchemy Base and links to the MySQL table states.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store States.
+        name (sqlalchemy String): The name of the State.
+        cities (sqlalchemy relationship): The State-City relationship.
+    """
+    storage = getenv("HBNB_TYPE_STORAGE")
+
+    if storage is None:
+        storage = "fs"
+
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    if storage == 'fs':
+        @property
+        def cities(self):
+            """return the cities of the current state"""
+            var = models.storage.all()
+            lista = []
+            result = []
+            for key in var:
+                city = key.replace('.', ' ')
+                city = shlex.split(city)
+                if (city[0] == 'City'):
+                    lista.append(var[key])
+            for elem in lista:
+                if (elem.state_id == self.id):
+                    result.append(elem)
+            return (result)
+
+    if storage == 'db':
+        cities = relationship('City', backref='state', cascade='all, delete')
