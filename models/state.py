0216be6619e40f2196825d04b8f7186be3e59d3d
Denzel18rb <74129875rb@gmail.com>
2024-02-13 16:59:27 -0500

    updated file state

--- models/state.py
+++ models/state.py
@@ -1,40 +1,35 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.city import City
-from models.base_model import BaseModel
-from models.base_model import Base
-from sqlalchemy import Column, String
+"""This is the state class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from os import getenv
+from sqlalchemy import Column, Integer, String
 import models
+from models.city import City
 import shlex
 
 
 class State(BaseModel, Base):
-    """
-    Represents a state for a MySQL database.
-
-    Inherits from SQLAlchemy Base and links to the MySQL table states.
-
+    """This is the class for State
     Attributes:
-        __tablename__ (str): The name of the MySQL table to store States.
-        name (sqlalchemy String): The name of the State.
-        cities (sqlalchemy relationship): The State-City relationship.
+        name: input name
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.storage = getenv("HBNB_TYPE_STORAGE", default="fs")
-
-        if self.storage == 'fs':
-            @property
-    
-            def cities(self):
-                """Return the list of City objects linked to the current State"""
-                all_cities = models.storage.all()
-                return [city for city in all_cities.values() if isinstance(city, models.City) and city.state_id == self.id]
-
-        elif self.storage == 'db':
-            cities = relationship('City', backref='state', cascade='all, delete')
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
