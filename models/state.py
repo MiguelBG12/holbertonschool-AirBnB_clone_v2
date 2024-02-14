dd787bd9763717bd849be249ccdaa51184deed33
MiguelBG12 <miguel.bg12@gmail.com>
2024-02-14 10:50:57 -0500

    Fix: State has a correct cities getter when the engine is FileStorage

--- models/state.py
+++ models/state.py
@@ -3,27 +3,29 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import os
+from os import getenv
 import models
 
 
-if os.getenv("HBNB_TYPE_STORAGE") == "db":
+if getenv('HBNB_TYPE_STORAGE') == 'db':
     class State(BaseModel, Base):
         """ State class """
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="all, delete")
 
+        __tablename__ = 'states'
+
+        name = Column(String(128), nullable=False)
+        from models.city import City
+        cities = relationship("City", backref='state', cascade='all, delete')
 else:
     class State(BaseModel):
-        name = ""
+        """ Use this option in case we use file_storage """
+        name = ''
 
         @property
         def cities(self):
-            "return the list of instances from City"
             from models.city import City
-            list_cities = []
+            new_cities = []
             for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    list_cities.append(city)
-            return list_cities
+                if hasattr(city, 'state_id') and city.state_id == self.id:
+                    new_cities.append(city)
+            return new_cities
